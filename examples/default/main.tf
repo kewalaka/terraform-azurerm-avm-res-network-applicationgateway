e356c11eb9db450d9fa094d6a7dd92555749afb3
Stu <kewalaka@gmail.com>
2024-05-06 19:21:06 +1200

    initial version (#1)
    * feat: initial version
    * chore: linting fixes
    * chore: use github runner workflow
    * fix: public IP sku
    * chore: fix hashicorp example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.74"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
   }
 }
 
@@ -17,19 +13,19 @@ provider "azurerm" {
 }
 
 
-## Section to provide a random Azure region for the resource group
-# This allows us to randomize the region for the resource group.
-module "regions" {
-  source  = "Azure/regions/azurerm"
-  version = "~> 0.3"
-}
+# ## Section to provide a random Azure region for the resource group
+# # This allows us to randomize the region for the resource group.
+# module "regions" {
+#   source  = "Azure/regions/azurerm"
+#   version = "~> 0.3"
+# }
 
-# This allows us to randomize the region for the resource group.
-resource "random_integer" "region_index" {
-  max = length(module.regions.regions) - 1
-  min = 0
-}
-## End of section to provide a random Azure region for the resource group
+# # This allows us to randomize the region for the resource group.
+# resource "random_integer" "region_index" {
+#   max = length(module.regions.regions) - 1
+#   min = 0
+# }
+# ## End of section to provide a random Azure region for the resource group
 
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
@@ -39,8 +35,40 @@ module "naming" {
 
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
-  location = module.regions.regions[random_integer.region_index.result].name
+  location = "AustraliaEast"
   name     = module.naming.resource_group.name_unique
+}
+
+resource "azurerm_virtual_network" "this" {
+  address_space       = ["10.254.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  address_prefixes     = ["10.254.0.0/24"]
+  name                 = module.naming.subnet.name_unique
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+
+resource "azurerm_public_ip" "this" {
+  allocation_method   = "Static"
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.public_ip.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "Standard"
+}
+
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.this.name}-beap"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.this.name}-feip"
+  frontend_port_name             = "${azurerm_virtual_network.this.name}-feport"
+  http_setting_name              = "${azurerm_virtual_network.this.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.this.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.this.name}-rqrt"
 }
 
 # This is the module call
@@ -52,8 +80,57 @@ module "test" {
   # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
   # ...
   location            = azurerm_resource_group.this.location
-  name                = "TODO" # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
+  name                = module.naming.application_gateway.name_unique
   resource_group_name = azurerm_resource_group.this.name
 
-  enable_telemetry = var.enable_telemetry # see variables.tf
+  backend_address_pool = [{
+    name = local.backend_address_pool_name
+  }]
+
+  backend_http_settings = [{
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }]
+
+  frontend_ip_configuration = [{
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.this.id
+  }]
+
+  frontend_port = [{
+    name = local.frontend_port_name
+    port = 80
+  }]
+
+  gateway_ip_configuration = [{
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.this.id
+  }]
+
+  http_listener = [{
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }]
+
+  request_routing_rule = [{
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }]
+
+  sku = {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
 }
